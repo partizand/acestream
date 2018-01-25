4a2acd711d396fcf687641017fa8c0d75e3ef6cf
partizand <elegaunte@gmail.com>
2018-01-25 14:50:35 +0300

    tar in tmp, add script_dir Not tested!

--- ubuntu/ace-install.sh
+++ ubuntu/ace-install.sh
@@ -12,33 +12,31 @@ INSTALL_DIR="/opt/acestream";
 ACE_ARH_FILE="acestream_3.1.16_ubuntu_16.04_x86_64.tar.gz";
 #-------------------------------------------------------
 
-# install dependices
+SCRIPT_DIR=$(readlink -f $(dirname $0));
 
+# install dependices
 sudo apt-get update -y; 
 sudo apt-get -y install python-setuptools;
 sudo apt-get -y install python-m2crypto;
 sudo apt-get -y install python-apsw;
 
 # Create user and group
-
 sudo adduser --system --no-create-home --shell /bin/false --group acestream;
 
 # load acestream-engine
-wget  -o - http://dl.acestream.org/linux/${ACE_ARH_FILE} ;
+wget  -O /tmp/${ACE_ARH_FILE} http://dl.acestream.org/linux/${ACE_ARH_FILE} ;
 sudo mkdir -p ${INSTALL_DIR}
-sudo tar -C ${INSTALL_DIR} --strip=1 -vxf ${ACE_ARH_FILE};
-rm ./${ACE_ARH_FILE}
+sudo tar -C ${INSTALL_DIR} --strip=1 -vxf /tmp/${ACE_ARH_FILE};
+rm /tmp/${ACE_ARH_FILE}
 
-# dir rights
+# dir owner
 sudo chown acestream:acestream ${INSTALL_DIR}
 sudo chown -R acestream:acestream ${INSTALL_DIR}/*
 
 # add service
-
-#sudo cp --update ./acestream.service /lib/systemd/system;
-sed "s=/opt/acestream=${INSTALL_DIR}=" ./acestream.service > ./new-acestream.service;
-sudo mv ./new-acestream.service /lib/systemd/system/acestream.service;
-sudo cp --update ./start-service ${INSTALL_DIR};
+sed "s=/opt/acestream=${INSTALL_DIR}=" ${SCRIPT_DIR}/acestream.service > /tmp/acestream.service;
+sudo mv /tmp/acestream.service /lib/systemd/system/acestream.service;
+sudo cp --update ${SCRIPT_DIR}/start-service ${INSTALL_DIR};
 sudo chmod +x ${INSTALL_DIR}/start-service
 
 # Enable and start service
